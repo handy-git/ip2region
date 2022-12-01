89a7e825ba7195d5005f4b0ddbc87385053b5d0d
韩帅 <shuai.han@ersoft.cn>
2022-12-01 11:29:45 +0800

    1.0.3 升级jar和添加是否显示省市后缀的配置

--- src/main/java/cn/handyplus/region/hook/PlaceholderUtil.java
+++ src/main/java/cn/handyplus/region/hook/PlaceholderUtil.java
@@ -3,6 +3,7 @@ package cn.handyplus.region.hook;
 import cn.handyplus.lib.core.StrUtil;
 import cn.handyplus.region.Ip2region;
 import cn.handyplus.region.constants.IpConstants;
+import cn.handyplus.region.util.ConfigUtil;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.OfflinePlayer;
 
@@ -62,13 +63,16 @@ public class PlaceholderUtil extends PlaceholderExpansion {
         if ("national".equals(identifier)) {
             return plugin.getConfig().getString(identifier, "0".equals(national) ? "未知" : national);
         }
+        boolean removeProvinceAndCity = ConfigUtil.CONFIG.getBoolean("removeProvinceAndCity");
         // %ip2region_provincial%
         if ("provincial".equals(identifier)) {
-            return plugin.getConfig().getString(identifier, "0".equals(provincial) ? "未知" : provincial);
+            String provincialStr = "0".equals(provincial) ? "未知" : provincial;
+            return plugin.getConfig().getString(identifier, removeProvinceAndCity ? provincialStr.replace("省", "") : provincialStr);
         }
         // %ip2region_municipal%
         if ("municipal".equals(identifier)) {
-            return plugin.getConfig().getString(identifier, "0".equals(municipal) ? "未知" : municipal);
+            String municipalStr = "0".equals(municipal) ? "未知" : municipal;
+            return plugin.getConfig().getString(identifier, removeProvinceAndCity ? municipalStr.replace("市", "") : municipalStr);
         }
         // %ip2region_serviceProvider%
         if ("serviceProvider".equals(identifier)) {
